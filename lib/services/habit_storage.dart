7f5d4a9c2405a35aae422092fbdab7ebb6977f4f
KaustubhPatil3 <kaustubhpatil.sknsits.comp>
2026-01-21 20:13:22 +0530

    Add daily reset logic for habits

--- lib/services/habit_storage.dart
+++ lib/services/habit_storage.dart
@@ -5,6 +5,8 @@ class HabitStorage {
   static final Box _box = Hive.box('habitsBox');
 
   static List<Habit> loadHabits() {
+    _checkDailyReset();
+
     final List data = _box.get('habits', defaultValue: []);
 
     return data.map((e) {
@@ -28,5 +30,27 @@ class HabitStorage {
     }).toList();
 
     _box.put('habits', data);
+    _box.put('lastDate', _today());
+  }
+
+  static void _checkDailyReset() {
+    final String today = _today();
+    final String? lastDate = _box.get('lastDate');
+
+    if (lastDate != today) {
+      final List data = _box.get('habits', defaultValue: []);
+
+      for (var h in data) {
+        h['completedToday'] = false;
+      }
+
+      _box.put('habits', data);
+      _box.put('lastDate', today);
+    }
+  }
+
+  static String _today() {
+    final now = DateTime.now();
+    return '${now.year}-${now.month}-${now.day}';
   }
 }
